aca5bfaf211f394b2b3948b95f600eba99039c5b
BrentKivell <brentkivell28@gmail.com>
2023-02-21 13:09:53 -0600

    Tuesday

--- js/index.js
+++ js/index.js
@@ -3,8 +3,16 @@ console.log("JS file is loaded correctly!");
 
 $("button").ready(function () {
     $("button").hover(function(){
-        $(this).css("background-color", "#7E184B");
-        }, function(){
         $(this).css("background-color", "#F47E20");
+        }, function(){
+        $(this).css("background-color", "#7E184B");
       });
     });
+
+    $("li").ready(function () {
+      $("li").hover(function(){
+          $(this).css("color", "#7E184B");
+          }, function(){
+          $(this).css("color", "#white");
+        });
+      });
